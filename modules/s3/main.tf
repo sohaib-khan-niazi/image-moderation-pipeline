8bca5511655575c2bc1ce45bc4a22b03c1ae43dc
sohaib-khan-niazi <sohaibkhanniazi1997@gmail.com>
2025-09-03 23:03:41 +0500

    Add Terraform configuration for S3 module

---
+++ modules/s3/main.tf
@@ -0,0 +1,60 @@
+resource "aws_s3_bucket" "input" {
+  bucket = var.input_bucket_name
+  tags   = var.tags
+}
+
+resource "aws_s3_bucket_public_access_block" "input" {
+  count  = var.block_public ? 1 : 0
+  bucket = aws_s3_bucket.input.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "input" {
+  bucket = aws_s3_bucket.input.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_versioning" "input" {
+  bucket = aws_s3_bucket.input.id
+  versioning_configuration {
+    status = var.enable_versioning ? "Enabled" : "Suspended"
+  }
+}
+
+data "aws_iam_policy_document" "tls_only" {
+  statement {
+    sid    = "DenyInsecureTransport"
+    effect = "Deny"
+
+    principals {
+      type        = "AWS"    
+      identifiers = ["*"]
+    }
+
+    actions = ["s3:*"]
+    resources = [
+      aws_s3_bucket.input.arn,
+      "${aws_s3_bucket.input.arn}/*"
+    ]
+
+    condition {
+      test     = "Bool"
+      variable = "aws:SecureTransport"
+      values   = ["false"]
+    }
+  }
+}
+
+resource "aws_s3_bucket_policy" "tls_only" {
+  count  = var.force_ssl ? 1 : 0
+  bucket = aws_s3_bucket.input.id
+  policy = data.aws_iam_policy_document.tls_only.json
+}
---
+++ modules/s3/variables.tf
@@ -0,0 +1,24 @@
+variable "input_bucket_name" { 
+  type = string 
+}
+
+variable "enable_versioning" { 
+  type = bool
+  default = false 
+}
+
+variable "block_public" { 
+  type = bool   
+  default = true 
+}
+
+variable "force_ssl" { 
+  type = bool   
+  default = true 
+}
+
+variable "tags" { 
+  type = map(string) 
+  default = {} 
+}
+
